9810f9da75bb74513827d1f74cd595c926220f7d
Neha Bhole <nehasbhole@gmail.com>
2023-11-16 18:40:37 -0500

    build out second page - firstname, lastname input boxes

--- src/SignUp/Step2.tsx
+++ src/SignUp/Step2.tsx
@@ -1,13 +1,21 @@
 import { StepProps } from "./types";
 import Button from "../components/Atoms/Buttons/Button";
+import LabeledInput from "components/Molecules/LabeledInput/LabeledInput";
 
 const Step2 = ({ step, setStep }: StepProps) => {
   return (
     <div>
-      <h1>Step {step}</h1>
+      <h1>Hey User</h1>
+      <h1>Welcome to Gather</h1>
+
+      <p>You are now logged in as email</p>
+      <LabeledInput 
+        label="First Name * "
+        input={{name: "firstName", value: "", placeholder: "User"}}
+        />
       <Button onClick={() => setStep(step + 1)}> Confirm </Button>
     </div>
-  );
-};
+  )
+}
 
 export default Step2;
